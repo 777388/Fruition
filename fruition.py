6b1094ad3af30306689c625ff949eae000c3d1c8
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-25 16:37:38 -0700

    Add files via upload

---
+++ fruition.py
@@ -0,0 +1,9 @@
+import sys
+import __future__
+__future__.generators.getMandatoryRelease.__sizeof__ #1-1/2.314
+__future__.generator_stop.compiler_flag.as_integer_ratio #1-1/2.314
+#read comments regarding sys.argv[1] in the ratio of 1-1/2.314
+try:
+    __future__._Feature.__getattribute__(sys.argv[1]) #1-1/2.314
+except:
+    __builtins__.BlockingIOError.__suppress_context__ #1-1/2.314
